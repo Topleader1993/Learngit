4edc54b0abed2e62bf189fe3eb6556c7ca6721a8
Alex Lee <liyongqiang1933@163.com>
2018-12-09 21:32:56 +0800

    C Program

---
+++ test.c
@@ -0,0 +1,17 @@
+    
+/*
+Project : Test
+Author  : Alex Lee 
+Location : Shanghai
+*/
+include "stdio.h"
+include "math.h"
+
+void main (void ){
+
+	int s;
+	scanf("%d/n",&s);
+	printf("%d/n",s);
+	return ;
+
+}
